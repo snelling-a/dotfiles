b99b0523964e43c8c4b44501c44f3a2dc6f53aea
Andrew Snelling <72226000+snelling-a@users.noreply.github.com>
2023-04-25 11:16:00 +0200

    Install script/Brewfile (#24)
    * Move $HOME files out of project root
    
    * Install script
    
    * Brewfile
    
    * Update PATH for brew
    
    * Update cspell.json

---
+++ scripts/install.sh
@@ -0,0 +1,108 @@
+#!/usr/bin/env bash
+
+timestamp=$(date +"%Y_%m_%d_%T")
+backup_dir=$HOME/.backup_${timestamp}
+
+backup() {
+	target="$1"
+
+	[ -e "$target" ] && [ ! -L "$target" ] && mv "$target" "$backup_dir"
+}
+
+link_file() {
+	file=$(realpath "$1")
+	target=${HOME}/$(basename "$1")
+
+	backup "$target"
+
+	ln -fnsv "$file" "$target"
+}
+
+link_dir() {
+	dir=$(realpath "$1")
+	target="${2:=HOME/.config}"
+
+	ln -fnsv "$dir" "$target"
+}
+
+is_file() {
+	[ -f "$1" ]
+}
+
+is_dir() {
+	[ -d "$1" ]
+}
+
+create_symlinks() {
+	for f in ./HOME/.* ./local/.*; do
+		is_file "$f" && link_file "$f"
+	done
+}
+
+link_config_directories() {
+	for dir in ./config/*; do
+		is_dir "$dir" && link_dir "$dir"
+	done
+}
+
+check_for_backups() {
+	if [ "$(ls -A "$backup_dir")" ]; then
+		echo "Take action $backup_dir is not Empty"
+
+		ls -la "$backup_dir"
+	else
+		rm -rf "$backup_dir"
+	fi
+}
+
+source_shell() {
+	if [ -f "$HOME/.zshrc" ]; then
+		source "$HOME/.zshenv"
+		source "$HOME/.zshrc"
+	fi
+	if [ -f "$HOME/.bashrc" ]; then
+		source "$HOME/.bashrc"
+	fi
+}
+
+brew_install() {
+	if ! command -v brew >/dev/null; then
+		echo "Installing Homebrew ..."
+		/bin/bash -c \
+			"$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+	fi
+
+	brew bundle install
+}
+
+generate_completions() {
+	competion_dir="$DOTFILES/zsh/completions"
+	wezterm shell-completion --shell zsh >"$competion_dir/_wezterm"
+
+	bw completion --shell=zsh >"$competion_dir/_bw"
+
+	curl https://raw.githubusercontent.com/gokcehan/lf/master/etc/lf.zsh -o "$competion_dir/_lf"
+}
+
+clone_notes() {
+	if [ ! -d "$NOTES" ]; then
+		echo "Cloning notes to $NOTES"
+		git clone https://github.com/snelling-a/notes.git "$NOTES"
+	fi
+}
+
+mkdir "$backup_dir"
+
+create_symlinks
+
+link_config_directories
+
+source_shell
+
+brew_install
+
+gerenate_completions
+
+clone_notes
+
+check_for_backups
