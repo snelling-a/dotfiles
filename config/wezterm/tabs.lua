fc89e1f17ae224bed4cfcbffb69377f32e037357
snelling-a <72226000+snelling-a@users.noreply.github.com>
2023-08-17 08:10:32 +0200

    fix: wezterm tabs

--- config/wezterm/tabs.lua
+++ config/wezterm/tabs.lua
@@ -2,38 +2,40 @@ local wezterm = require("wezterm")
 local colors = require("const").colors
 local nerdfonts = wezterm.nerdfonts
 
-local function basename(s)
+local function get_basename(s)
 	return string.gsub(s, "(.*[/\\])(.*)", "%2")
 end
 
 local function get_process(tab)
-	local title = tab or "zsh"
-	local process = basename(tab.active_pane.foreground_process_name)
-
-	local process_icons = require("const").process_icons
 	if string.match(tab.active_pane.title, "Copy mode:") then
 		return nerdfonts.oct_copy
 	end
 
-	return wezterm.format(process_icons[process] or {
+	local process = get_basename(tab.active_pane.foreground_process_name)
+	local icon = require("const").process_icons[process]
+
+	local fallback = {
 		{
 			Foreground = {
 				Color = colors.foreground,
 			},
 		},
 		{
-			Text = string.format("[%s]", title),
+			Text = string.format("[%s]", process or "zsh"),
 		},
-	})
+	}
+
+	return wezterm.format(icon or fallback)
 end
 
 local function get_current_working_dir(tab)
 	local current_dir = tab.active_pane.current_working_dir
 	local HOME_DIR = string.format("file://%s", os.getenv("HOME"))
 
-	return current_dir == HOME_DIR and nerdfonts.md_tilde or string.format(" %s", basename(current_dir))
+	return current_dir == HOME_DIR and nerdfonts.md_tilde or string.format(" %s", get_basename(current_dir))
 end
 
+---@diagnostic disable-next-line: unused-local
 wezterm.on("format-tab-title", function(tab, _tabs, _panes, _config, _hover, _max_width)
 	local zoomed = tab.active_pane.is_zoomed and wezterm.pad_left(nerdfonts.md_magnify, 3) or ""
 
