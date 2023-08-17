b3bd4f623ed5d70eae9a0d459d4e179df0bfe562
snelling-a <72226000+snelling-a@users.noreply.github.com>
2023-08-17 12:34:50 +0200

    style: spacing

--- zsh/fzf-git.sh
+++ zsh/fzf-git.sh
@@ -53,17 +53,21 @@ fbranch() {
 # fco_preview - checkout git branch/tag, with a preview showing the commits between the tag/branch and HEAD
 fco() {
 	local branches target
+
 	branches=$(
 		git --no-pager branch --all \
 			--format="%(if)%(HEAD)%(then)%(else)%(if:equals=HEAD)%(refname:strip=3)%(then)%(else)%1B[0;34;1mbranch%09%1B[m%(refname:short)%(end)%(end)" |
 			sed '/^$/d'
 	) || return
+
 	target=$(
-		(
-			echo "$branches"
-		) |
-			fzf --no-hscroll --no-multi -n 2 \
-				--ansi --preview="git --no-pager log -150 --pretty=format:%s '..{2}'"
+		(echo "$branches") |
+			fzf --no-hscroll \
+				--no-multi \
+				-n 2 \
+				--ansi \
+				--preview="git --no-pager log -150 --pretty=format:%s '..{2}'"
 	) || return
+
 	git checkout "$(awk '{print $2}' <<<"$target")"
 }
