5066cdfc718fbe7bbb372af9431990c0e6dcf7a1
snelling-a <72226000+snelling-a@users.noreply.github.com>
2023-08-18 13:37:35 +0200

    style(wezterm): make font an extendable module

--- config/wezterm/font.lua
+++ config/wezterm/font.lua
@@ -1,19 +1,16 @@
 local wezterm = require("wezterm")
 
-local font = "Iosevka Term SS15"
-local icon_font = {
-	family = "Symbols Nerd Font Mono",
-}
+local Font = {}
 
-return {
-	font = wezterm.font_with_fallback({
-		{
-			family = font,
-			assume_emoji_presentation = false,
-		},
-		icon_font,
-		use_cap_height_to_scale_fallback_fonts = true,
-	}),
-	font_size = 16.0,
-	font_rules = {},
-}
+Font.font = wezterm.font_with_fallback({
+	{
+		family = "Iosevka Term SS15",
+	},
+	{
+		family = "Symbols Nerd Font Mono",
+	},
+})
+
+Font.font_size = 16.0
+
+return Font
