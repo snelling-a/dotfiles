ef2f56a204b7117311e45051a297f4d5c326754c
snelling-a <72226000+snelling-a@users.noreply.github.com>
2023-10-13 13:47:20 +0200

    style(wezterm): DRY key maps

--- config/wezterm/keys.lua
+++ config/wezterm/keys.lua
@@ -1,377 +1,102 @@
 local wezterm = require("wezterm")
 local act = wezterm.action
 
+---@param key string
+---@param action function|string?
+---@param mods string? "LEADER" | "CTRL" | "SHIFT" | "ALT" | "SUPER" | "CMD"
+---@return table
+local function get_key(key, action, mods)
+	return {
+		action = action,
+		key = key,
+		mods = mods,
+	}
+end
 local M = {}
 
 local tab_keys = {}
 
 for i = 1, 8 do
-	table.insert(tab_keys, {
-		key = tostring(i),
-		mods = "SUPER",
-		action = act.ActivateTab(i - 1),
-	})
+	table.insert(tab_keys, get_key(tostring(i), act.ActivateTab(i - 1), "SUPER"))
 end
 
 M.keys = {
-	{
-		key = "+",
-		mods = "SUPER|SHIFT",
-		action = act.IncreaseFontSize,
-	},
-	{
-		key = "-",
-		mods = "SUPER|SHIFT",
-		action = act.DecreaseFontSize,
-	},
-	{
-		key = "0",
-		mods = "SUPER",
-		action = act.ResetFontSize,
-	},
-	{
-		key = "DownArrow",
-		mods = "LEADER",
-		action = act.AdjustPaneSize({
-			"Down",
-			5,
-		}),
-	},
-	{
-		key = "LeftArrow",
-		mods = "LEADER",
-		action = act.AdjustPaneSize({
-			"Left",
-			5,
-		}),
-	},
-	{
-		key = "RightArrow",
-		mods = "LEADER",
-		action = act.AdjustPaneSize({
-			"Right",
-			5,
-		}),
-	},
-	{
-		key = "Space",
-		mods = "LEADER",
-		action = act.RotatePanes("Clockwise"),
-	},
-	{
-		key = "Space",
-		mods = "LEADER|SHIFT",
-		action = act.RotatePanes("CounterClockwise"),
-	},
-	{
-		key = "Tab",
-		mods = "CTRL",
-		action = act.ActivateTabRelative(1),
-	},
-	{
-		key = "Tab",
-		mods = "CTRL|SHIFT",
-		action = act.ActivateTabRelative(-1),
-	},
-	{
-		key = "UpArrow",
-		mods = "LEADER",
-		action = act.AdjustPaneSize({
-			"Up",
-			5,
-		}),
-	},
-	{
-		key = "[",
-		mods = "LEADER",
-		action = act.ActivateCopyMode,
-	},
-	{
-		key = "a",
-		mods = "CTRL|SHIFT",
-		action = act.EmitEvent("toggle-leader"),
-	},
-	{
-		key = "b",
-		mods = "CTRL|SHIFT",
-		action = act.EmitEvent("toggle-opacity"),
-	},
-	{
-		key = "c",
-		mods = "LEADER",
-		action = act.SpawnTab("CurrentPaneDomain"),
-	},
-	{
-		key = "c",
-		mods = "SUPER",
-		action = act.CopyTo("Clipboard"),
-	},
-	{
-		key = "e",
-		mods = "CTRL|SHIFT",
-		action = act.EmitEvent("toggle-ligature"),
-	},
-	{
-		key = "f",
-		mods = "SUPER",
-		action = act.Search({
-			CaseInSensitiveString = "",
-		}),
-	},
-	{
-		key = "h",
-		mods = "CTRL|SHIFT",
-		action = act.Search({
-			Regex = "[a-f0-9]{6,}",
-		}),
-	},
-	{
-		key = "h",
-		mods = "LEADER",
-		action = act.ActivatePaneDirection("Left"),
-	},
-	{
-		key = "h",
-		mods = "LEADER|SHIFT",
-		action = act.AdjustPaneSize({
-			"Left",
-			5,
-		}),
-	},
-	{
-		key = "j",
-		mods = "LEADER",
-		action = act.ActivatePaneDirection("Down"),
-	},
-	{
-		key = "j",
-		mods = "LEADER|SHIFT",
-		action = act.AdjustPaneSize({
-			"Down",
-			5,
-		}),
-	},
-	{
-		key = "k",
-		mods = "LEADER",
-		action = act.ActivatePaneDirection("Up"),
-	},
-	{
-		key = "k",
-		mods = "LEADER|SHIFT",
-		action = act.AdjustPaneSize({
-			"Up",
-			5,
-		}),
-	},
-	{
-		key = "k",
-		mods = "SUPER",
-		action = act.Multiple({
-			act.ClearScrollback("ScrollbackAndViewport"),
-			act.SendKey({
-				key = "L",
-				mods = "CTRL",
-			}),
-		}),
-	},
-	{
-		key = "l",
-		mods = "SUPER",
-		action = act.ShowLauncher,
-	},
-	{
-		key = "l",
-		mods = "SUPER|SHIFT",
-		action = act.ShowDebugOverlay,
-	},
-	{
-		key = "l",
-		mods = "LEADER",
-		action = act.ActivatePaneDirection("Right"),
-	},
-	{
-		key = "l",
-		mods = "LEADER|SHIFT",
-		action = act.AdjustPaneSize({
-			"Right",
-			5,
-		}),
-	},
-	{
-		key = "m",
-		mods = "CMD",
-		action = act.DisableDefaultAssignment,
-	},
-	{
-		key = "n",
-		mods = "SUPER",
-		action = act.SpawnWindow,
-	},
-	{
-		key = "q",
-		mods = "LEADER",
-		action = act.PaneSelect({
-			mode = "SwapWithActive",
-		}),
-	},
-	{
-		key = "q",
-		mods = "SUPER",
-		action = act.QuitApplication,
-	},
-	{
-		key = "s",
-		mods = "LEADER",
-		action = act.SplitVertical({
-			domain = "CurrentPaneDomain",
-		}),
-	},
-	{
-		key = "t",
-		mods = "SUPER",
-		action = act.SpawnTab("CurrentPaneDomain"),
-	},
-	{
-		key = "v",
-		mods = "LEADER",
-		action = act.SplitHorizontal({
-			domain = "CurrentPaneDomain",
-		}),
-	},
-	{
-		key = "v",
-		mods = "SUPER",
-		action = act.PasteFrom("Clipboard"),
-	},
-	{
-		key = "w",
-		mods = "SUPER",
-		action = act.CloseCurrentTab({
-			confirm = true,
-		}),
-	},
-	{
-		key = "x",
-		mods = "LEADER",
-		action = act.CloseCurrentPane({
-			confirm = true,
-		}),
-	},
-	{
-		key = "x",
-		mods = "LEADER|SHIFT",
-		action = act.CloseCurrentTab({
-			confirm = true,
-		}),
-	},
-	{
-		key = "z",
-		mods = "LEADER",
-		action = act.TogglePaneZoomState,
-	},
+	get_key(
+		"!",
+		wezterm.action_callback(function(_win, pane)
+			pane:move_to_new_tab()
+		end),
+		"LEADER|SHIFT"
+	),
+	get_key("+", act.IncreaseFontSize, "SUPER|SHIFT"),
+	get_key("-", act.DecreaseFontSize, "SUPER|SHIFT"),
+	get_key("0", act.ResetFontSize, "SUPER"),
+	get_key("DownArrow", act.AdjustPaneSize({ "Down", 5 }), "LEADER"),
+	get_key("LeftArrow", act.AdjustPaneSize({ "Left", 5 }), "LEADER"),
+	get_key("RightArrow", act.AdjustPaneSize({ "Right", 5 }), "LEADER"),
+	get_key("Space", act.RotatePanes("Clockwise"), "LEADER"),
+	get_key("Space", act.RotatePanes("CounterClockwise"), "LEADER|SHIFT"),
+	get_key("Tab", act.ActivateTabRelative(-1), "CTRL|SHIFT"),
+	get_key("Tab", act.ActivateTabRelative(1), "CTRL"),
+	get_key("UpArrow", act.AdjustPaneSize({ "Up", 5 }), "LEADER"),
+	get_key("[", act.ActivateCopyMode, "LEADER"),
+	get_key("a", act.EmitEvent("toggle-leader"), "CTRL|SHIFT"),
+	get_key("b", act.EmitEvent("toggle-opacity"), "CTRL|SHIFT"),
+	get_key("c", act.CopyTo("Clipboard"), "SUPER"),
+	get_key("c", act.SpawnTab("CurrentPaneDomain"), "LEADER"),
+	get_key("e", act.EmitEvent("toggle-ligature"), "CTRL|SHIFT"),
+	get_key("f", act.Search({ CaseInSensitiveString = "" }), "SUPER"),
+	get_key("h", act.ActivatePaneDirection("Left"), "LEADER"),
+	get_key("h", act.Search({ Regex = "[a-f0-9]{6,}" }), "CTRL|SHIFT"),
+	get_key("j", act.ActivatePaneDirection("Down"), "LEADER"),
+	get_key("k", act.ActivatePaneDirection("Up"), "LEADER"),
+	get_key(
+		"k",
+		act.Multiple({ act.ClearScrollback("ScrollbackAndViewport"), act.SendKey(get_key("L", nil, "CTRL")) }),
+		"SUPER"
+	),
+	get_key("l", act.ActivatePaneDirection("Right"), "LEADER"),
+	get_key("l", act.ShowDebugOverlay, "SUPER|SHIFT"),
+	get_key("l", act.ShowLauncher, "SUPER"),
+	get_key("m", act.DisableDefaultAssignment, "CMD"),
+	get_key("n", act.SpawnWindow, "SUPER"),
+	get_key("q", act.PaneSelect({ mode = "SwapWithActive" }), "LEADER"),
+	get_key("q", act.QuitApplication, "SUPER"),
+	get_key("s", act.SplitVertical({ domain = "CurrentPaneDomain" }), "LEADER"),
+	get_key("t", act.SpawnTab("CurrentPaneDomain"), "SUPER"),
+	get_key("v", act.PasteFrom("Clipboard"), "SUPER"),
+	get_key("v", act.SplitHorizontal({ domain = "CurrentPaneDomain" }), "LEADER"),
+	get_key("w", act.CloseCurrentTab({ confirm = true }), "SUPER"),
+	get_key("x", act.CloseCurrentPane({ confirm = true }), "LEADER"),
+	get_key("x", act.CloseCurrentTab({ confirm = true }), "LEADER|SHIFT"),
+	get_key("z", act.TogglePaneZoomState, "LEADER"),
+	get_key("{", act.MoveTabRelative(-1), "SHIFT|ALT"),
+	get_key("}", act.MoveTabRelative(1), "SHIFT|ALT"),
 
 	-- KEY TABLES
-	{
-		key = "r",
-		mods = "LEADER",
-		action = act.ActivateKeyTable({
-			name = "resize_pane",
-			one_shot = false,
-		}),
-	},
+	get_key("r", act.ActivateKeyTable({ name = "resize_pane", one_shot = false }), "LEADER"),
+
 	table.unpack(tab_keys),
 }
 
 local resize_pane = {
-	{
-		key = "DownArrow",
-		action = act.AdjustPaneSize({
-			"Down",
-			1,
-		}),
-	},
-	{
-		key = "Escape",
-		action = "PopKeyTable",
-	},
-	{
-		key = "LeftArrow",
-		action = act.AdjustPaneSize({
-			"Left",
-			1,
-		}),
-	},
-	{
-		key = "RightArrow",
-		action = act.AdjustPaneSize({
-			"Right",
-			1,
-		}),
-	},
-	{
-		key = "UpArrow",
-		action = act.AdjustPaneSize({
-			"Up",
-			1,
-		}),
-	},
-	{
-		key = "h",
-		action = act.AdjustPaneSize({
-			"Left",
-			1,
-		}),
-	},
-	{
-		key = "j",
-		action = act.AdjustPaneSize({
-			"Down",
-			1,
-		}),
-	},
-	{
-		key = "k",
-		action = act.AdjustPaneSize({
-			"Up",
-			1,
-		}),
-	},
-	{
-		key = "l",
-		action = act.AdjustPaneSize({
-			"Right",
-			1,
-		}),
-	},
+	get_key("DownArrow", act.AdjustPaneSize({ "Down", 1 }), nil),
+	get_key("Escape", "PopKeyTable", nil),
+	get_key("h", act.AdjustPaneSize({ "Left", 1 }), nil),
+	get_key("j", act.AdjustPaneSize({ "Down", 1 }), nil),
+	get_key("k", act.AdjustPaneSize({ "Up", 1 }), nil),
+	get_key("l", act.AdjustPaneSize({ "Right", 1 }), nil),
+	get_key("LeftArrow", act.AdjustPaneSize({ "Left", 1 }), nil),
+	get_key("RightArrow", act.AdjustPaneSize({ "Right", 1 }), nil),
+	get_key("UpArrow", act.AdjustPaneSize({ "Up", 1 }), nil),
 }
 
 local copy_mode_remap = {
-	{
-		key = "b",
-		mods = "SHIFT",
-		action = act.CopyMode("MoveBackwardWord"),
-	},
-	{
-		key = "h",
-		mods = "SHIFT",
-		action = act.CopyMode("MoveToStartOfLineContent"),
-	},
-	{
-		key = "l",
-		mods = "SHIFT",
-		action = act.CopyMode("MoveToEndOfLineContent"),
-	},
-	{
-		key = "w",
-		mods = "NONE",
-		action = act.CopyMode("MoveForwardWord"),
-	},
-	{
-		key = "y",
-		mods = "NONE",
-		action = act.CopyTo("ClipboardAndPrimarySelection"),
-	},
+	get_key("b", act.CopyMode("MoveBackwardWord"), "SHIFT"),
+	get_key("h", act.CopyMode("MoveToStartOfLineContent"), "SHIFT"),
+	get_key("l", act.CopyMode("MoveToEndOfLineContent"), "SHIFT"),
+	get_key("w", act.CopyMode("MoveForwardWord"), "NONE"),
+	get_key("y", act.CopyTo("ClipboardAndPrimarySelection"), "NONE"),
 }
 
 local copy_mode = nil
@@ -380,7 +105,7 @@ if wezterm.gui then
 end
 
 for key, _ in ipairs(copy_mode_remap) do
-	table.insert(copy_mode, copy_mode_remap[key])
+	table.insert(copy_mode or {}, copy_mode_remap[key])
 end
 
 M.key_tables = {
