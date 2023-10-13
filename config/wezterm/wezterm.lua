b86b590ab60784317d4297d6b80b2172556ea5aa
snelling-a <72226000+snelling-a@users.noreply.github.com>
2023-10-13 13:51:24 +0200

    style(wezterm): sort fields

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -31,23 +31,23 @@ M.macos_window_background_blur = 20
 M.switch_to_last_active_tab_when_closing_tab = true
 M.term = "wezterm"
 M.visual_bell = {
-	fade_in_function = "EaseIn",
 	fade_in_duration_ms = 70,
-	fade_out_function = "EaseOut",
+	fade_in_function = "EaseIn",
 	fade_out_duration_ms = 70,
+	fade_out_function = "EaseOut",
 }
 M.window_background_opacity = 0.5
 M.window_close_confirmation = "NeverPrompt"
 M.window_decorations = "RESIZE"
 M.window_frame = {
+    active_titlebar_bg = colors.background,
 	font_size = 15,
-	active_titlebar_bg = colors.background,
 }
 M.window_padding = {
+	bottom = 0,
 	left = 0,
 	right = 0,
 	top = 0,
-	bottom = 0,
 }
 
 -- github user/repo
