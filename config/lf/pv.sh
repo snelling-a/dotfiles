f7842ed764dc7a0140ad97cbd7514dbf08eb7c56
snelling-a <72226000+snelling-a@users.noreply.github.com>
2023-10-18 10:31:52 +0200

    fix(lf): preview
    most filetypes would appear in the preview window but would not open

--- config/lf/pv.sh
+++ config/lf/pv.sh
@@ -5,18 +5,17 @@ set -eu
 thumbnail="/tmp/thumbnail.png"
 
 file="${1}"
-width="${2}"
-height="${3}"
+width="${2:-$(tput cols)}"
+height="${3:-$(tput lines)}"
 
 [ -L "${file}" ] && file="$(readlink "$file")"
 
-jq_json() {
-	jq --color-output . "${1}"
-}
-
 preview() {
 	local input="${1:-$file}"
-	chafa "$input" -f sixel -s "$((width - 2))x$height" | sed 's/#/\n#/g'
+	chafa "$file" -f sixel -s "$((width - 2))x$height" | sed 's/#/\n#/g' ||
+		local result=$?
+
+	return "$result"
 }
 
 view_binary() {
@@ -24,6 +23,8 @@ view_binary() {
 
 	hexyl -n "$len" --border none "$file" ||
 		hexdump -n "$len" -C "$file"
+
+	return $?
 }
 
 view_opendocument() {
@@ -35,6 +36,7 @@ view_opendocument() {
 		glow -s dark -w "${width}" \
 			<(pandoc "${file}" --to=markdown || odt2txt "${file}")
 	fi
+	return $?
 }
 
 view_sourcecode() {
@@ -49,6 +51,8 @@ view_sourcecode() {
 		--theme=base16 \
 		--wrap=auto ||
 		cat "${input}" || view_binary
+
+	return $?
 }
 
 view_sqlite() {
@@ -56,6 +60,7 @@ view_sqlite() {
 	sqlite3 "$file" ".tables"
 	echo -e "\n# \e[1;37mSCHEMA\e[0m\n"
 	sqlite3 "$file" ".schema"
+	return $?
 }
 
 not_implemented() {
@@ -63,61 +68,68 @@ not_implemented() {
 	file="$(readlink "$file")"
 }
 
-case "$file" in
-*.jpg | *.jpeg | *.png | *.bmp | *.webp)
-	preview "$file" "$@"
-	;;
-*.7z)
-	7zz l "$file"
-	;;
-*.avi | *.gif | *.mp4 | *.mkv | *.webm)
-	not_implemented
-	;;
-*.csv)
-	csview --style=Rounded "${file}"
-	;;
-*.mp3 | *.flac | *.opus)
-	not_implemented
-	;;
-*.tar* | *.tgz | *.xz)
-	tar tf "$file"
-	;;
-*.bzip | *.bzip2 | *.bp | *.bz2 | *.dmg | *.gz)
-	not_implemented
-	;;
-*.deb)
-	ar -tv "${file}"
-	;;
-*.json*)
-	sed '/^[[:blank:]]*#/d;s/\/\/.*//' "${file}" | jq --color-output .
-	;;
-*.md)
-	glow -s dark -w "${width}" "${file}"
-	;;
-*.pdf)
-	gs -o "$thumbnail" -sDEVICE=png48 -dLastPage=1 "$file" >/dev/null
-	preview "$thumbnail" "$@"
-	;;
-*.plist)
-	plutil -p "${file}"
-	;;
-*.rar)
-	rar l "$file"
-	;;
-*.svg)
-	convert "$file" "$thumbnail"
-	preview "$thumbnail" "$@"
-	;;
-*.zip)
-	unzip -l "${file}"
-	;;
-*)
+main() {
+	case "${file}" in
+	*.7z)
+		7zz l "$file"
+		;;
+	*.avi | *.gif | *.mp4 | *.mkv | *.webm)
+		not_implemented
+		;;
+	*.bzip | *.bzip2 | *.bp | *.bz2 | *.dmg | *.gz)
+		not_implemented
+		;;
+	*.csv)
+		csview --style=Rounded "${file}"
+		;;
+	*.deb)
+		ar -tv "${file}"
+
+		return $?
+		;;
+	*.jpg | *.JPG | *.png | *.PNG)
+		preview "$file" || view_binary
+		;;
+	*.json*)
+		sed '/^[[:blank:]]*#/d;s/\/\/.*//' "${file}" | jq --color-output . && return $?
+		;;
+	*.md)
+		glow -s dark -w "${width}" "${file}" && return $?
+		;;
+	*.pdf)
+		gs -o "$thumbnail" -sDEVICE=png48 -dLastPage=1 "$file" >/dev/null
+
+		preview "$thumbnail" "$@"
+		;;
+	*.plist)
+		plutil -p "${file}" && return $?
+		;;
+	*.rar)
+		rar l "$file"
+
+		return $?
+		;;
+	*.tar* | *.tgz | *.xz)
+		tar tf "$file"
+		;;
+	*.txt)
+		view_sourcecode "$file"
+		;;
+	*.zip)
+		unzip -l "${file}"
+
+		return $?
+		;;
+	esac
+
 	case "$(file -b --mime-type "${file}")" in
-	inode/directory | application/x-directory) lsd --tree --color=always --icon=always --icon-theme=fancy "$file" ;;
 	application/gzip | application-x-xz)
 		tar tvf "${file}"
 		;;
 	application/octet-stream | application/x-*-binary | application/x-*executable)
+		view_binary
+		;;
+	application/pdf)
 		view_binary
 		;;
 	application/vnd.*-officedocument* | application/vnd.*.opendocument*)
@@ -129,11 +141,19 @@ case "$file" in
 	application/x-terminfo | text/x-bytecode.python)
 		view_binary
 		;;
+	image/*)
+		preview || view_binary
+		;;
+	inode/directory | application/x-directory)
+		lsd --tree --color=always --icon=always --icon-theme=fancy "$file"
+		;;
+
 	*)
-		view_sourcecode "$file"
+		view_sourcecode
 		;;
 	esac
-	;;
-esac
 
-return 127 # nonzero retcode required for lf previews to reload#
+	return $?
+}
+
+main "$file"
