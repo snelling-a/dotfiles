17ce5442a78f3d8c473e5f422dd23b21f1993fc5
snelling-a <72226000+snelling-a@users.noreply.github.com>
2023-10-24 20:22:40 +0300

    feat(shell): list_env function

--- zsh/functions.sh
+++ zsh/functions.sh
@@ -4,5 +4,12 @@ print_path() {
 	tr : '\n' <<<"$PATH" | sort
 }
 
+list_env() {
+	local var
+	var=$(printenv | cut -d= -f1 | fzf --prompt 'env:' --preview='printenv {}') &&
+		echo "$var=$(printenv "$var")" &&
+		unset var
+}
+
 # pull gitignore template
 gi() { curl -sLw n "https://www.toptal.com/developers/gitignore/api/$*"; }
