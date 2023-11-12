1ca6df0f629cf2583c8ddd8e2b92197d7233168e
snelling-a <72226000+snelling-a@users.noreply.github.com>
2023-11-12 16:14:54 +0100

    style(npm): remove unneccesary comment/alias

--- shell/npm.sh
+++ shell/npm.sh
@@ -8,16 +8,10 @@ alias nukenode="rm -rf node_modules && rm package-lock.json && npm install"
 alias npmG="npm i -g "
 
 # Install and save to dependencies in your package.json
-# npms is used by https://www.npmjs.com/package/npms
 alias npmS="npm i -S "
 
 # Install and save to dev-dependencies in your package.json
-# npmd is used by https://github.com/dominictarr/npmd
 alias npmD="npm i -D "
-
-# Execute command from node_modules folder based on current directory
-# i.e npmE gulp
-alias npmE='PATH="$(npm bin)":"$PATH"'
 
 # Check which npm modules are outdated
 alias npmO="npm outdated"
