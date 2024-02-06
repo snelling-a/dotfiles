1f4ab00d174de0cab5b8902636b415006388cf62
snelling-a <72226000+snelling-a@users.noreply.github.com>
2024-02-06 08:20:25 +0100

    chore(wezterm): show_update_window is deprecated
    will be removed in future release

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -49,7 +49,6 @@ M.leader = keys.leader
 M.macos_window_background_blur = 20
 M.send_composed_key_when_right_alt_is_pressed = false
 M.show_new_tab_button_in_tab_bar = false
-M.show_update_window = false
 M.switch_to_last_active_tab_when_closing_tab = true
 M.term = "wezterm"
 M.use_fancy_tab_bar = false
