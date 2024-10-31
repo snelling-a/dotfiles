21503ec1a16ee4dbe32b1bd2303bd0abdf264c70
snelling-a <72226000+snelling-a@users.noreply.github.com>
2024-10-31 08:50:22 +0100

    chore(brew): update packages

---
+++ config/yazi/init.lua
@@ -0,0 +1,59 @@
+function Linemode:size_and_mtime()
+	local time = math.floor(self._file.cha.modified or 0)
+	if time == 0 then
+		time = ""
+	elseif os.date("%Y", time) == os.date("%Y") then
+		time = os.date("%b %d %H:%M", time)
+	else
+		time = os.date("%b %d  %Y", time)
+	end
+
+	local size = self._file:size()
+	return ui.Line(string.format("%s %s", size and ya.readable_size(size) or "-", time))
+end
+
+require("starship"):setup()
+require("full-border"):setup()
+require("git"):setup()
+
+function Status:name()
+	local h = self._tab.current.hovered
+	if not h then
+		return ui.Line({})
+	end
+
+	local linked = ""
+	if h.link_to ~= nil then
+		linked = " -> " .. tostring(h.link_to)
+	end
+	return ui.Line(" " .. h.name .. linked)
+end
+
+Status:children_add(function()
+	local h = cx.active.current.hovered
+	if h == nil or ya.target_family() ~= "unix" then
+		return ui.Line({})
+	end
+
+	return ui.Line({
+		ui.Span(ya.user_name(h.cha.uid) or tostring(h.cha.uid)):fg("magenta"),
+		ui.Span(":"),
+		ui.Span(ya.group_name(h.cha.gid) or tostring(h.cha.gid)):fg("magenta"),
+		ui.Span(" "),
+	})
+end, 500, Status.RIGHT)
+
+function Status:percentage() end
+
+local function module_exists(name)
+	if package.loaded[name] then
+		return true
+	else
+		local status, _ = pcall(require, name)
+		return status
+	end
+end
+
+if module_exists("starship") then
+	print("THIS")
+end
