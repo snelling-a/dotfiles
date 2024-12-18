c45d421bf68a42dd3b143d11205769976f0ab505
snelling-a <72226000+snelling-a@users.noreply.github.com>
2024-12-18 23:26:21 +0100

    fix(yazi): update for breaking changes

--- config/yazi/plugins/arrow.yazi/init.lua
+++ config/yazi/plugins/arrow.yazi/init.lua
@@ -1,10 +1,10 @@
 --- @sync entry
 return {
-	entry = function(_, args)
+	entry = function(_, job)
 		---@diagnostic disable-next-line: no-unknown
 		local current = cx.active.current
 		---@type number
-		local new = (current.cursor + args[1]) % #current.files
+		local new = (current.cursor + job.args[1]) % #current.files
 		---@diagnostic disable-next-line: no-unknown
 		ya.manager_emit("arrow", { new - current.cursor })
 	end,
