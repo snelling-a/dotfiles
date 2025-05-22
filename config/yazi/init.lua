53f9f3497a7a5e6cc6cc9020ea57e711a3bc19d9
snelling-a <72226000+snelling-a@users.noreply.github.com>
2025-05-22 11:15:43 +0200

    fix(yazi): breaking changes

--- config/yazi/init.lua
+++ config/yazi/init.lua
@@ -1,35 +1,3 @@
-local function module_exists(name)
-	if package.loaded[name] then
-		return true
-	else
-		---@diagnostic disable-next-line: no-unknown
-		local status, _ = pcall(require, name)
-		return status
-	end
-end
-
-if not module_exists("starship") then
-	os.execute("ya pack -i")
-end
-
-require("full-border"):setup()
-require("git"):setup()
 require("starship"):setup()
-
----@diagnostic disable-next-line: no-unknown
-function Status:name()
-	---@type table
-	local h = self._tab.current.hovered
-	if not h then
-		---@type {Line: fun(arg: table|string)}
-		---@diagnostic disable-next-line: no-unknown
-		return ui.Line({})
-	end
-
-	local linked = ""
-	if h.link_to ~= nil then
-		linked = " -> " .. tostring(h.link_to)
-	end
-	---@diagnostic disable-next-line: no-unknown
-	return ui.Line(" " .. h.name .. linked)
-end
+require("full-border"):setup({ type = ui.Border.ROUNDED })
+require("git"):setup()
