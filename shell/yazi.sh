61c08c050fa944aa17b36f33531ee2fbe180ad42
snelling-a <72226000+snelling-a@users.noreply.github.com>
2026-03-25 15:52:30 +0100

    fix(yazi): outputting temp dir

--- shell/yazi.sh
+++ shell/yazi.sh
@@ -3,7 +3,6 @@
 function y() {
 	local tmp
 	tmp="$(mktemp -t "yazi-cwd.XXXXXX")"
-	$tmp cwd
 	yazi "$@" --cwd-file="$tmp"
 	if cwd="$(command cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
 		builtin cd -- "$cwd" || exit
